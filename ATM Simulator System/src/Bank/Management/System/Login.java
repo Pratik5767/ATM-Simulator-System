5acba315704f5b86e7f5cc327614a190b5905e74
Pratik5676 <pratikvsalunkhe924@gmail.com>
2024-10-08 13:07:36 +0530

    Login page

---
+++ ATM Simulator System/src/Bank/Management/System/Login.java
@@ -0,0 +1,14 @@
+package Bank.Management.System;
+
+public class Login {
+
+	Login() {
+		System.out.println("Welcome to Login page of ATM");
+	}
+	
+	public static void main(String[] args) {
+		
+		new Login();
+	}
+
+}
